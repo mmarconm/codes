d5952c6ca04b080ca1f5dc5cd5d0eca4a4f23a40
Marcelo Marcon <marconm.inf0@gmail.com>
2017-08-04 23:48:59 -0400

    added configs

---
+++ configs/ppa-icons.sh
@@ -0,0 +1,3 @@
+add-apt-repository --yes ppa:numix/ppa && add-apt-repository --yes ppa:moka/stable && add-apt-repository --yes ppa:tista/adapta && apt update -y && apt install -y numix-gtk-theme numix-icon-theme* moka-* adapta-*
+
+echo "Icons Installed"
