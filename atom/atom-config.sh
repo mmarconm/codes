b318adcaa18d9a25ed89dd189aa5a22c2b082952
Marcelo Marcon <marconm.inf0@gmail.com>
2017-10-12 19:32:43 -0400

    remove python installs from atom-config.sh and added terminator config to config folder

--- atom/atom-config.sh
+++ atom/atom-config.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo ">>> Installing pip packages for python3"
-python3 -m pip install --user -U -r requirements.txt
-echo ">>> Pip packages Installed"
-
-echo ">>> Installing Packages on Atom"
-apm install --packages-file apm-packages.log
-echo ">>> Packages Installed"
-
 echo ">>> Coping the Files"
 cp {keymap,config}.cson $HOME/.atom/
 echo ">>> Config Copied"
