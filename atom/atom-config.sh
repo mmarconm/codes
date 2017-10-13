5c54f666013cd930352ecd12a2a659fa5e273b28
Marcelo <marconm.inf0@gmail.com>
2017-10-13 04:46:49 +0000

    update

--- atom/atom-config.sh
+++ atom/atom-config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo ">>> Installing Packages on Atom"
+apm install --packages-file apm-packages.log
+echo ">>> Packages Installed"
+
 echo ">>> Coping the Files"
 cp {keymap,config}.cson $HOME/.atom/
 echo ">>> Config Copied"
