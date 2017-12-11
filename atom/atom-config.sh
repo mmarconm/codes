2debc25dc0ef7cb1cc9b835ab49c9b18e09bb0df
Marcelo Marcon <marconm.inf0@gmail.com>
2017-12-11 00:37:49 -0300

    update the atom installation

--- atom/atom-config.sh
+++ atom/atom-config.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
-echo ">>> Installing Packages on Atom"
-apm install --packages-file new-themes.log
-echo ">>> Packages Installed"
+function install_atom() {
 
-echo ">>> Coping the Files"
-cp {keymap,config}.cson $HOME/.atom/
-echo ">>> Config Copied"
+    echo ">>> Installing Packages on Atom"
+    apm install --packages-file new-themes.log
+    echo ">>> Packages Installed"
 
-echo ">>> Config Git"
-cp gitconfig $HOME/.gitconfig
+    echo ">>> Coping the Files"
+    cp {keymap,config}.cson $HOME/.atom/
+    echo ">>> Config Copied"
 
-echo ">>> Done !!"
+    echo ">>> Config Git"
+    cp gitconfig $HOME/.gitconfig
+
+    echo ">>> Done !!"
+    
+}
+
+echo ">>> Searching for a possible existing installation"
+
+if [ -d /home/$USER/.atom ]; then
+    echo "The folder Already Exists, what do you want to do"
+    echo "Type 1 to delete the folder and rebuild the atom installation"
+    echo "type 2 to exit"
+    read option
+
+    case $option in
+
+	1)
+	   rm /home/$USER/.atom -rf
+	   install_atom
+	   echo "Folder Deleted" ;;
+	
+	2)
+	    echo "Bye" ;;
+
+	*)
+	    echo "You must to choose 1 or 2 to install or exit" ;;
+    esac 
+
+else
+
+    install_atom
+
+fi
+
+
