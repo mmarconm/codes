5b4f6fe25ae1bf36e5a854f647948fb2e30b1f16
Marcelo Marcon <marcelo.marcon@ax4b.com>
2021-10-30 11:54:28 -0400

    removed some unused configuration files from config folder

--- configs/ppa-icons.sh
+++
@@ -1,3 +0,0 @@
-add-apt-repository --yes ppa:numix/ppa && add-apt-repository --yes ppa:moka/stable && add-apt-repository --yes ppa:tista/adapta && apt update -y && apt install -y numix-gtk-theme numix-icon-theme* moka-* adapta-*
-
-echo "Icons Installed"
